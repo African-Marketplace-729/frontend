37b95c912eda77051a3e32da9c50dc7f28b883b6
Beau Bradley <beaubradley@outlook.com>
2020-09-20 00:34:18 -0400

    cards cleanup

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import {Container, Row, Col} from 'reactstrap';
 import Card from './Card';
-export default class Cards extends React.Component{
+export default function Cards() {
 
-    render(){
-
-        return (
-            <section className="cards-container">
-                {console.log(this.props.userCards)}
-                <Container>
-                    <Row>
-                {this.props.userCards && this.props.userCards.map(item => {
-                    
-                    return (
-                            <Col md={3}>
-                                <Card key={item.id} item={item} />
-                            </Col>
-                    )
-                })}
-                    </Row>
-                </Container>
-            </section>
-        )
-    }
+    return (
+        <section className="cards-container">
+        </section>
+    )
 }

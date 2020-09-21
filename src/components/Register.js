20767b5fb87c7ac0edce3684ec2dfa22c841396c
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-09-21 13:47:11 -0500

    created basic schema for form validation

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import * as yup from "yup";
+import schema from "../validation/schema";
 
 const initialValues = {
   username: "",

a13a1c2bcfe51368618ac46967ca56bd880ea8bf
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-09-21 14:44:15 -0500

    implemented schema on both register and login forms, tested both as well

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,15 +1,43 @@
 import React, { useState, useEffect } from "react";
+import * as yup from "yup";
+import schema from "../validation/schema";
 
 const initialValues = {
   username: "",
   password: "",
 };
 
+const initialErrors = {
+  username: "",
+  password: "",
+};
+
 export default function Signin() {
   const [values, setValues] = useState(initialValues);
+  const [errors, setErrors] = useState(initialErrors);
+  const [disabled, setDisabled] = useState(true);
+
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(valid => {
+        setErrors({
+          ...errors,
+          [name]: "",
+        });
+      })
+      .catch(err => {
+        setErrors({
+          ...errors,
+          [name]: err.errors[0],
+        });
+      });
+  };
 
   const onChange = e => {
     const { name, value } = e.target;
+    validate(name, value);
     setValues({
       ...values,
       [name]: value,
@@ -19,6 +47,12 @@ export default function Signin() {
   const onSubmit = e => {
     e.preventDefault();
   };
+
+  useEffect(() => {
+    schema.isValid(values).then(valid => {
+      setDisabled(!valid);
+    });
+  }, [values]);
 
   return (
     <form className="signin-container" onSubmit={onSubmit}>
@@ -41,7 +75,11 @@ export default function Signin() {
           onChange={onChange}
         />
       </label>
-      <button>Log In</button>
+      <article>
+        <button disabled={disabled}>Log In</button>
+        <p>{errors.username}</p>
+        <p>{errors.password}</p>
+      </article>
     </form>
   );
 }

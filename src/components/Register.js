ecbf5908e955fae7cb560e9dd12f630a560199a8
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-09-21 12:31:45 -0500

    completed username and password inputs for register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,51 @@
 import React, { useState, useEffect } from "react";
 
+const initialValues = {
+  username: "",
+  password: "",
+};
+
 export default function Register() {
+  const [values, setValues] = useState(initialValues);
+  const [users, setUsers] = useState([]);
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newUser = {
+      username: values.username.trim(),
+      password: values.password.trim(),
+    };
+  };
+
   return (
-    <form className="register-container">
+    <form className="register-container" onSubmit={onSubmit}>
       <h2>Sign Up</h2>
       <label htmlFor="username">
-        Username:
-        <input type="text" name="username" />
+        Username
+        <input
+          type="text"
+          name="username"
+          value={values.username}
+          onChange={onChange}
+        />
+      </label>
+      <label htmlFor="password">
+        Password
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={onChange}
+        />
       </label>
     </form>
   );

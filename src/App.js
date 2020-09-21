9155cd3ab0f089f1ae122c055e48d4af86948f2b
Beau Bradley <beaubradley@outlook.com>
2020-09-21 14:55:46 -0400

    axios calls added

--- src/App.js
+++ src/App.js
@@ -29,8 +29,10 @@ function App() {
       <br/><br/><hr/><br/>
       <Link to='/register'>Create an account.</Link>
       <br/><br/><hr/><br/>
+
       {/* User Profile will live somewhere else. */}
       {/* But so it's visible, it is here        */}
+      
       <UserProfile />
     </div>
   );

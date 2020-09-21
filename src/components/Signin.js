a4350468e1dcd166a30d90c932c985a0b3d7062e
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-09-21 13:07:10 -0500

    completed and tested both forms, state is updating properly on both

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const initialValues = {
   username: "",
@@ -6,10 +6,42 @@ const initialValues = {
 };
 
 export default function Signin() {
-    const [values, setValues] = 
+  const [values, setValues] = useState(initialValues);
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
-    <form className="signin-container">
+    <form className="signin-container" onSubmit={onSubmit}>
       <h2>Log In</h2>
+      <label htmlFor="username">
+        Username
+        <input
+          type="text"
+          name="username"
+          value={values.username}
+          onChange={onChange}
+        />
+      </label>
+      <label htmlFor="password">
+        Password
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={onChange}
+        />
+      </label>
+      <button>Log In</button>
     </form>
   );
 }

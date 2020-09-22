cc3db669fb53d0a990ff021fa25e95a63b5ba200
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-09-22 12:28:17 -0500

    added category dropdown to add listing form

--- src/components/AddListing.js
+++ src/components/AddListing.js
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 
 const initialValues = {
-  listingName: "",
-  listingType: "",
-  merchantId: "",
+  name: "",
+  category: "",
+  subcategory: "",
   price: null,
-  status: "",
-  createdBy: "",
-  createdDate: "",
 };
 
 export default function AddListing() {
@@ -15,9 +12,26 @@ export default function AddListing() {
   return (
     <form>
       <h2>Add a Listing</h2>
-      <label htmlFor="listingName">
-        Listing Name
-        <input type="text" name="listingName" />
+      <label htmlFor="name">
+        Name
+        <input type="text" name="name" />
+      </label>
+      <label htmlFor="category">
+        Category
+        <select name="category" value={values.category}>
+          <option value="">--Select a Category--</option>
+          <option value="Animal Products">Animal Products</option>
+          <option value="Beans">Beans</option>
+          <option value="Cereals - Maize">Cereals - Maize</option>
+          <option value="Cereals - Rice">Cereals - Rice</option>
+          <option value="Cereals - Other">Cereals - Other</option>
+          <option value="Fruits">Fruits</option>
+          <option value="Peas">Peas</option>
+          <option value="Roots & Tubers">Roots & Tubers</option>
+          <option value="Seeds & Nuts">Seeds & Nuts</option>
+          <option value="Vegetables">Vegetables</option>
+          <option value="Other">Other</option>
+        </select>
       </label>
     </form>
   );

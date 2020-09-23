eaeaf65c9444e62065a1c575eb0ee98645f47c65
hussainabutt <68175777+hussainabutt@users.noreply.github.com>
2020-09-23 16:29:54 -0400

    UserProfileCreation done, profile display

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React from "react";
+import Cards from "./Cards";
 
-export default function UserProfile(){
-
-
-    return (
-        <div className='profile-container'>
-            <h1> User Profile Goes Here</h1>
-            <div>
-                <h3>Name:</h3>
-                <h3>Picture: </h3>
-                <h3>Member Since: </h3>
-                <h3>Location: </h3>
-                <h3>Listings: </h3>    
-            </div>
-        </div>
-    )
+export default function UserProfile() {
+  return (
+    <div className="profile-container">
+      <h1 className="ShopName">`${UserProfile.shopname}`</h1>
+      <div>
+        <hr />
+        <h3>Name: </h3>
+        <img
+          className="profilepic"
+          src={`${UserProfile.picture}`}
+          alt="Profile"
+        />
+        <hr />
+        <h3>Member Since: </h3>
+        <h3>Location: </h3>
+        <h3>Listings: </h3>
+        <Cards />
+      </div>
+    </div>
+  );
 }
